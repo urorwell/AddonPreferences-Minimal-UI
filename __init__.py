64462abb128cf9df5941c0dd7e5f1c0daefdc14e
noyogi <e.w.r@hotmail.com>
2021-05-26 18:01:00 -0700

    bl_info update

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 bl_info = {
-    "name" : "Addon Prefences - Minimal - UI",
-    "author" : "Noyogi",
-    "version" : (0,2),
-    "blender" : (2,92,0),
-    "location" : "Preferences > Addons",
+    "name": "Addon Prefences - Minimal - UI",
+    "author": "Noyogi",
+    "version": (0,2),
+    "blender": (2,92,0),
+    "location": "Preferences > Addons",
     "description": "Simplifies Addon Panel",
-    "warning" : "Only tested on 2.92, on Ubuntu 20.04",
-    "doc_url" : "https://github.com/urorwell/AddonPreferences-Minimal-UI",
-    "tracker_url" : "https://github.com/urorwell/AddonPreferences-Minimal-UI/issues",
-    "category" : "Interface",
+    "warning": "Only tested on 2.92, on Ubuntu 20.04",
+    "doc_url": "https://github.com/urorwell/AddonPreferences-Minimal-UI",
+    "tracker_url": "https://github.com/urorwell/AddonPreferences-Minimal-UI/issues",
+    "category": "Interface"
 }
 
 
